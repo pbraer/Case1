80bf6e948e5a8ce96129f33a4cf72a3d72253fab
darya752 <73088666+darya752@users.noreply.github.com>
2020-10-24 15:36:04 +0300

    triangle

--- main.py
+++ main.py
@@ -33,3 +33,39 @@ def square(x, y, side, angle, color):
     turtle.left(angle)
     turtle.end_fill()
     turtle.done()
+
+import turtle as t
+
+def triangle(x, y, leg, angle, color):
+    '''
+
+    Function, drawing triangle.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param leg: a leg Fof a triangle
+    :param hyp: a hypotenuse of a triangle
+    :param angle: the angle of rotation of a triangle
+    :param color: the color of a triangle
+    :return: None
+    '''
+
+leg = 200
+x = -100
+y = -100
+hyp = ((leg ** 2 + leg ** 2) ** 0.5)
+color = ('#FEDD14')
+
+t.penup()
+t.goto(x, y)
+t.begin_fill()
+t.pencolor(color)
+t.pendown()
+t.forward(leg)
+t.left(90)
+t.forward(leg)
+t.left(135)
+t.forward(hyp)
+t.fillcolor(color)
+t.end_fill()
+t.hideturtle()
+t.mainloop()
